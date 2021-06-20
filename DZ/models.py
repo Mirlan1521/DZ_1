1e376e588c3b7502afcbff95c40b145a0b723cba
@Kuruchbekovich <mirlan_33-3@mail.ru>
2021-06-21 00:34:56 +0600

    first DZ

---
+++ DZ/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    name = models.CharField(max_length=255, verbose_name='Имя')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    title = models.CharField('Название', max_length=255)
+    description = models.CharField('Описание', max_length=255)
+    price = models.IntegerField('Цена', default=0)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    class Meta:
+        verbose_name = 'Описание'
+        verbose_name_plural = 'Описание'
+
+    text = models.CharField('Текст',max_length=255)
+    product = models.ForeignKey(Product,verbose_name= 'Продукт', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.text
